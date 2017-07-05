4f90ee03cc775c382e342313bb92d32864df5a04
Johann Werner <johann@wunder.org>
2017-07-05 20:21:46 +0200

    comments

--- mytaxi/API.m
+++ mytaxi/API.m
@@ -15,6 +15,11 @@
 + (void)fetchListWithCarsCompletion:(void (^)(CarListModel *response,
                                                    NSError *error))completion {
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    
+    /* NSURLRequest objects represent a URL load request in a manner independent of protocol and URL scheme.
+     NSURLRequest encapsulates two basic data elements of a load request: the URL to load, and the policy to use 
+     when consulting the URL content cache made available by the implementation.
+     */
     NSString *endpoint = [NSString
                           stringWithFormat:@"%@%@", APIBaseURL, @"/car2go/vehicles.json"];
     [request setURL:[NSURL URLWithString:endpoint]];
@@ -22,15 +27,51 @@
     [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
     
+    /*
+     The NSURLSession class and related classes provide an API for downloading content. 
+     This API provides a rich set of delegate methods for supporting authentication and gives
+     your app the ability to perform background downloads when your app is not running or, in iOS, while your app is suspended.
+     
+     An NSURLSessionConfiguration object defines the behavior and policies to use when uploading and downloading data using an
+     
+     NSURLSession
+     object. When uploading or downloading data, creating a configuration object is always the first step you must take. You 
+     use this object to configure the timeout values, caching policies, connection requirements, and other types of information
+     that you intend to use with your NSURLSession object.
+     
+     An ephemeral session configuration object is similar to a default session configuration object except that the corresponding 
+     session object does not store caches, credential stores, or any session-related data to disk. Instead, session-related data 
+     is stored in RAM. The only time an ephemeral session writes data to disk is when you tell it to write the contents of a URL to a file.
+     
+     
+
+     */
     NSURLSession *session =
     [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration
-                                            defaultSessionConfiguration]];
+                                            defaultSessionConfiguration]]; // Creates a session with the specified session configuration.
     
+    /*
+     Creates a task that retrieves the contents of a URL based on the specified URL request object, and calls a handler upon completion.
+     
+     
+     */
     [[session dataTaskWithRequest:request
                 completionHandler:^(NSData *data, NSURLResponse *response,
-                                    NSError *error) {
+                                    NSError *error) { /*The NSURLResponse class encapsulates the metadata associated with the response
+                                                       to a a URL load request in a manner independent of protocol and URL scheme.*/
                     if (data != nil) {
-                        NSDictionary *responseDictionary =
+                        NSDictionary *responseDictionary = //You use the NSJSONSerialization class to convert JSON to Foundation objects
+                        
+                        /* why kNilOptions
+                         
+                         NSJSONReadingMutableContainers
+                         Specifies that arrays and dictionaries are created as mutable objects.
+                         NSJSONReadingMutableLeaves
+                         Specifies that leaf strings in the JSON object graph are created as instances of NSMutableString.
+                         NSJSONReadingAllowFragments
+                         Specifies that the parser should allow top-level objects that are not an instance of NSArray or NSDictionary.
+                         
+                         */
                         [NSJSONSerialization JSONObjectWithData:data
                                                         options:kNilOptions
                                                           error:&error];
@@ -46,6 +87,21 @@
                     }
                 }] resume];
 }
+
+
+/*&error
+ 
+ NSError's fields are not settable. its interface is opaque and immutable. therefore, if you were to alloc/init an error, 
+ the client could not populate it. thus, the approach is to return a new error (if applicable), also saving you from 
+ allocs in every case.
+ 
+ since the return type is already specified by the method, and the error is optional, it makes sense to take this 
+ approach to returning the error in idiomatic c syntax.
+ 
+ iow, the client may neither mutate what you pass nor return the error using :(NSError *)outError, 
+ so the "out parameter" form :(NSError**)outError is applied.
+ 
+ */
 
 
 
--- mytaxi/UIImage+Tint.m
+++ mytaxi/UIImage+Tint.m
@@ -11,34 +11,96 @@
 @implementation UIImage (Tint)
 
 - (UIImage *)tintedImage:(UIColor *)color {
-    
     UIGraphicsBeginImageContextWithOptions(self.size, NO, [[UIScreen mainScreen] scale]);
+/*
+ While the context created by this function is the current context, you can call the 
+    UIGraphicsGetImageFromCurrentImageContext function to retrieve an
+    image object based on the current contents of the context. 
+ When you are done modifying the context, you must call the UIGraphicsEndImageContext function
+ to clean up the bitmap drawing environment and remove the graphics context from the top of the 
+ context stack. You should not use the UIGraphicsPopContext function
+ to remove this type of context from the stack.
+ */
     CGContextRef context = UIGraphicsGetCurrentContext();
+    /*Returns the current graphics context.
+     
+     The current graphics context is nil by default. Prior to calling its drawRect: method, view objects
+     push a valid context onto the stack, making it current.
+     If you are not using a UIView object to do your drawing, however, you must push a valid context onto the stack manually
+     using the UIGraphicsPushContext function.
+
+     
+     */
     
-    CGContextTranslateCTM(context, 0, self.size.height);
-    CGContextScaleCTM(context, 1.0, (CGFloat) -1.0);
+    CGContextTranslateCTM(context, 0, self.size.height);//Image is not visible IR
+    /*Changes the origin of the user coordinate system in a context.
+     
+     c
+     A graphics context.
+     tx
+     The amount to displace the x-axis of the coordinate space, in units of the user space, of the specified context.
+     ty
+     The amount to displace the y-axis of the coordinate space, in units of the user space, of the specified context.
+*/
+    
+    CGContextScaleCTM(context, 1.0, -1.0); //Image is not visible IR
+/*
+ Changes the scale of the user coordinate system in a context.
+ Parameters
+ c
+ A graphics context.
+ sx
+ The factor by which to scale the x-axis of the coordinate space of the specified context.
+ sy
+ The factor by which to scale the y-axis of the coordinate space of the specified context.
+*/
     
     CGRect rect = CGRectMake(0, 0, self.size.width, self.size.height);
     
-    CGContextSetBlendMode(context, kCGBlendModeNormal);
-    [[UIColor blackColor] setFill];
-    CGContextFillRect(context, rect);
+    // kCGBlendModeNormal Paints the source image samples over the background image samples.
     
-    CGContextSetBlendMode(context, kCGBlendModeNormal);
+    // draw black background to preserve color of transparent pixels
+//    CGContextSetBlendMode(context, kCGBlendModeNormal);
+    //Sets how sample values are composited by a graphics context.
+    
+//    [[UIColor blackColor] setFill];
+//    CGContextFillRect(context, rect);
+    /*Paints the area contained within the provided rectangle, using the fill color in the current graphics state.
+     The current path is cleared as a side effect of calling this function.
+     */
+    
+    // draw original image
+//    CGContextSetBlendMode(context, kCGBlendModeNormal);
     CGContextDrawImage(context, rect, self.CGImage);
+    /*Draws an image into a graphics context.
+     The image is scaled—disproportionately, if necessary—to fit the bounds specified by the rect parameter.
+     */
     
-    CGContextSetBlendMode(context, kCGBlendModeSourceAtop);
+    // color over image with tint color
+    CGContextSetBlendMode(context, kCGBlendModeSourceAtop);// R = S*Da + D*(1 - Sa)
     [color setFill];
-    CGContextFillRect(context, rect);
+    CGContextFillRect(context, rect);//Image will be black if removed
     
-    CGContextSetBlendMode(context, kCGBlendModeDestinationIn);
-    CGContextDrawImage(context, rect, self.CGImage);
+    // mask by alpha values of original image
+//    CGContextSetBlendMode(context, kCGBlendModeDestinationIn);
+//    CGContextDrawImage(context, rect, self.CGImage);
     
     UIImage *tintedImage = UIGraphicsGetImageFromCurrentImageContext();
+    
+    /*Returns an image based on the contents of the current bitmap-based graphics context.
+     You should call this function only when a bitmap-based graphics context is the current graphics context. 
+     If the current context is nil or was not created by a call to UIGraphicsBeginImageContext, this function returns nil.
+     This function may be called from any thread of your app.
+     Returns
+     A image object containing the contents of the current bitmap graphics context.
+     */
     UIGraphicsEndImageContext();
+    /*
+     you must call the UIGraphicsEndImageContext function to clean up the bitmap drawing environment 
+     */
     
     return tintedImage;
-    
 }
+
 
 @end
